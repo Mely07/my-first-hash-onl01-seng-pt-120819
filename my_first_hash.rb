c764671a42acdbc23abbd8d08fe2e1e4ec0b2779
Damely Tineo <damely.tineo07@gmail.com>
2019-12-20 21:36:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,6 +38,6 @@ def adding
   # return the shipping_manifest hash below
   shipping_manifest = {"muskets" => 2, "gun powder" => 4}
   shipping_manifest["muskets"] = "New Value"
-  shipping_manifest["gun powder"] = "New Value". 
+  shipping_manifest["gun powder"] = "New Value" 
   return shipping_manifest
 end
